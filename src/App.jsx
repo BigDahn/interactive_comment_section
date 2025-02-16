24c8321bebfd379a5329a230e76e8725192449b9
Akinbo Olaoluwa Daniel <big_dahn@dahn>
2025-02-16 13:46:17 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Layout from "./components/Layout"
+
+const App = () => {
+  return (
+    <div className='grid place-items-center m-auto pt-2'>
+      <Layout/>
+    </div>
+  )
+}
+
+export default App
